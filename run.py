d37a1aadcc14a171095b2e526fcd3ea00f28c760
Mundino Hamure <mundinoabete@gmail.com>
2024-09-12 15:58:15 +0000

    add TaskManager class

--- run.py
+++ run.py
@@ -10,3 +10,15 @@ class Task:
     def __repr__(self):
         status = "Done" if self.completed else "Pending"
         return f"[{status}] {self.description}"
+
+# TaskManager class to manage the list of tasks
+
+class TaskManager:
+    def __init__(self):
+        self.tasks=[]
+
+    def add_task(self, description):
+        task = Task(description)
+        self.tasks.append(task)
+        print(f"Task '{description}' added.")
+        
