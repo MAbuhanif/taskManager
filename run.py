f38d5f196af2ef6e931a59fba7bfe1618dcd47b1
Mundino Hamure <mundinoabete@gmail.com>
2024-09-12 16:01:45 +0000

    add function for completed tasks

--- run.py
+++ run.py
@@ -29,3 +29,9 @@ class TaskManager:
             for idx, task in enumerate(self.tasks, 1):
                 print(f"{idx}. {task}")
         
+    def mark_task_completed(self, task_number):
+        if 0 < task_number <= len(self.tasks):
+            self.tasks[task_number - 1].completed = True
+            print(f"Task {task_number} marked as completed.")
+        else:
+            print("Invalid task number.")
